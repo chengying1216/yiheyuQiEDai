384cee4553da387329036ea423af29015cc39220
czhang <222222>
2016-11-01 18:32:16 +0800

    第二天

---
+++ app/src/main/java/com/yiheyu/qiedai/view/activity/ForgetPswActivity.java
@@ -0,0 +1,14 @@
+package com.yiheyu.qiedai.view.activity;
+
+import android.os.Bundle;
+
+import com.yiheyu.qiedai.R;
+
+public class ForgetPswActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_psw);
+    }
+}
