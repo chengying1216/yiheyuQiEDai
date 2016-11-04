2371bef80e8bd432b4a1f6d1dd29fdff28ef392d
czhang <222222>
2016-11-04 09:19:14 +0800

    第三天

---
+++ app/src/main/java/com/yiheyu/qiedai/view/activity/baidu/LocationActivity.java
@@ -0,0 +1,15 @@
+package com.yiheyu.qiedai.view.activity.baidu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yiheyu.qiedai.R;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
